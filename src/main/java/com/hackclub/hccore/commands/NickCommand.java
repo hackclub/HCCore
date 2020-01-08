33e29e5debc2b13ed72a36d42443926f42d30c9d
Victor Truong <me@ifvictr.com>
2020-01-08 01:03:58 -0800

    Disallow nicknames longer than 8 characters

--- src/main/java/com/hackclub/hccore/commands/NickCommand.java
+++ src/main/java/com/hackclub/hccore/commands/NickCommand.java
@@ -28,6 +28,14 @@ public class NickCommand implements CommandExecutor {
         }
 
         String newName = String.join(" ", args);
+        // FIX: Prevent other players from being kicked. It's 8 instead of 16 because the AFK
+        // suffix takes up 8 characters. Will need to figure out how to bypass the 16 character
+        // limit.
+        if (newName.length() > 8) {
+            player.sendMessage(ChatColor.RED + "Your nickname can’t be longer than 8 characters");
+            return true;
+        }
+
         this.plugin.getDataManager().getData(player).setNickname(newName);
         player.sendMessage(
                 ChatColor.GREEN + "Your nickname was set to " + ChatColor.AQUA + newName);
