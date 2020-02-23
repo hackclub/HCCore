9b19c3879be6dfdfc72f3a8422b96ddb58e216f1
Victor Truong <me@ifvictr.com>
2020-02-22 16:24:12 -0800

    Add ability to /ping other players

--- src/main/java/com/hackclub/hccore/commands/PingCommand.java
+++ src/main/java/com/hackclub/hccore/commands/PingCommand.java
@@ -1,13 +1,18 @@
 package com.hackclub.hccore.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import com.hackclub.hccore.HCCorePlugin;
+import com.hackclub.hccore.PlayerData;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
-public class PingCommand implements CommandExecutor {
+public class PingCommand implements TabExecutor {
     private final HCCorePlugin plugin;
 
     public PingCommand(HCCorePlugin plugin) {
@@ -16,22 +21,59 @@ public class PingCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "You must be a player to use this");
+        // /ping
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "You must be a player to use this");
+                return true;
+            }
+
+            Player player = (Player) sender;
+            int ping = this.getPing(player);
+            // Failed for some reason
+            if (ping == -1) {
+                player.sendMessage(ChatColor.RED + "Failed to get your ping");
+                return true;
+            }
+
+            sender.sendMessage("Your ping is " + ping + "ms");
             return true;
         }
 
-        Player player = (Player) sender;
-        int ping = this.getPing(player);
-        // Failed for some reason
-        if (ping == -1) {
-            player.sendMessage(ChatColor.RED + "Failed to get ping");
-            return true;
-        }
+        // /ping [player]
+        Player targetPlayer = this.plugin.getServer().getPlayerExact(args[0]);
+        if (targetPlayer != null) {
+            PlayerData data = this.plugin.getDataManager().getData(targetPlayer);
+            int ping = this.getPing(targetPlayer);
+            // Failed for some reason
+            if (ping == -1) {
+                sender.sendMessage(
+                        ChatColor.RED + "Failed to get " + data.getUsableName() + "’s ping");
+                return true;
+            }
 
-        player.sendMessage("Your ping is " + ping + "ms");
+            sender.sendMessage(data.getUsableName() + "’s ping is " + ping + "ms");
+        } else {
+            sender.sendMessage(ChatColor.RED + "No online player with that name was found");
+        }
 
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias,
+            String[] args) {
+        List<String> completions = new ArrayList<String>();
+        if (args.length == 1) {
+            for (Player player : this.plugin.getServer().getOnlinePlayers()) {
+                if (StringUtil.startsWithIgnoreCase(player.getName(), args[0])) {
+                    completions.add(player.getName());
+                }
+            }
+        }
+
+        Collections.sort(completions);
+        return completions;
     }
 
     private int getPing(Player player) {
