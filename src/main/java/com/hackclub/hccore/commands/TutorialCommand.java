4d03fb3385c899250ce9704836d405ca74a4bd4a
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-04-30 19:45:57 -0500

    capitalized name of static array message

--- src/main/java/com/hackclub/hccore/commands/TutorialCommand.java
+++ src/main/java/com/hackclub/hccore/commands/TutorialCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class TutorialCommand implements CommandExecutor {
-    private static final String[] message = {ChatColor.RED + "Greetings! Welcome to the Hack Club vanilla Minecraft server!", "You can use /nick to set your nick name and /color to set your chat and name colors", ChatColor.RED + "Rules:\n", "Be nice.",  "No griefing.", "Don't build within 1000 blocks of spawn.", "If you want to contribute to plugin development head on over to the GitHub: " + ChatColor.RED + "https://github.com/hackclub/HCCore/. ", "Type /tutorial to see this at any time."};
+    private static final String[] MESSAGE = {ChatColor.RED + "Greetings! Welcome to the Hack Club vanilla Minecraft server!", "You can use /nick to set your nick name and /color to set your chat and name colors", ChatColor.RED + "Rules:", "Be nice.",  "No griefing.", "Don't build within 1000 blocks of spawn.", "If you want to contribute to plugin development head on over to the GitHub: " + ChatColor.RED + "https://github.com/hackclub/HCCore/. ", "Type /tutorial to see this at any time."};
 
     private final HCCorePlugin plugin;
 
@@ -24,8 +24,8 @@ public class TutorialCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
-        for (String i : TutorialCommand.message) {
-            sender.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + i);
+        for (String message : TutorialCommand.MESSAGE) {
+            sender.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + message);
             }
 
         return true;
