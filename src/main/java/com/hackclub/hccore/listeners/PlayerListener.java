a03534028ca2d397388a09b899ea7cd129a5351e
Steggy <javascriptcubing999@gmail.com>
2020-08-13 08:11:57 -0400

    Update PlayerListener.java

--- src/main/java/com/hackclub/hccore/listeners/PlayerListener.java
+++ src/main/java/com/hackclub/hccore/listeners/PlayerListener.java
@@ -3,6 +3,7 @@ package com.hackclub.hccore.listeners;
 import com.hackclub.hccore.HCCorePlugin;
 import com.hackclub.hccore.PlayerData;
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import org.bukkit.Bukkit;
