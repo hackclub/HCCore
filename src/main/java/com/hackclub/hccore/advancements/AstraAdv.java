bc5a4dfef1321c3459dd8174618f5542a245b404
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-03-02 21:41:17 -0500

    Automatically grant Ad Astra advancement when player reaches the minimum y level (10000)

--- src/main/java/com/hackclub/hccore/advancements/AstraAdv.java
+++ src/main/java/com/hackclub/hccore/advancements/AstraAdv.java
@@ -8,6 +8,8 @@ import com.fren_gor.ultimateAdvancementAPI.util.AdvancementKey;
 import com.fren_gor.ultimateAdvancementAPI.util.CoordAdapter;
 import com.hackclub.hccore.HCCorePlugin;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class AstraAdv extends BaseAdvancement {
     static AdvancementDisplayBuilder<AdvancementDisplay.Builder, AdvancementDisplay> displayBuilder = new AdvancementDisplay.Builder(Material.FIREWORK_ROCKET, "Ad " +
@@ -17,8 +19,17 @@ public class AstraAdv extends BaseAdvancement {
             .showToast()
             .description("Reach outer space and touch the stars");
     static int maxProgression = 1;
+    static int minY = 10000;
 
     public AstraAdv(HCCorePlugin plugin, Advancement root, AdvancementKey key, CoordAdapter adapter) {
         super(key.getKey(), displayBuilder.coords(adapter, key).build(), root, maxProgression);
+
+        registerEvent(PlayerMoveEvent.class, e -> {
+            if (e.getPlayer().getWorld().getEnvironment() == World.Environment.NORMAL) {
+                if (e.getPlayer().getLocation().getY() > minY) {
+                    incrementProgression(e.getPlayer());
+                }
+            }
+        });
     }
 }
