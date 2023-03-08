d20e1e57ceaeccb9804e9938fa14f77214d2e82b
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-03-07 19:51:17 -0500

    Fix the color being included as text in nickname response message

--- src/main/java/com/hackclub/hccore/commands/NickCommand.java
+++ src/main/java/com/hackclub/hccore/commands/NickCommand.java
@@ -3,7 +3,7 @@ package com.hackclub.hccore.commands;
 import com.hackclub.hccore.HCCorePlugin;
 import com.hackclub.hccore.PlayerData;
 import net.kyori.adventure.text.Component;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,13 +22,15 @@ public class NickCommand implements CommandExecutor {
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd,
       @NotNull String alias, String[] args) {
     if (!(sender instanceof Player player)) {
-      sender.sendMessage(ChatColor.RED + "You must be a player to use this");
+      sender.sendMessage(
+          Component.text("You must be a player to use this").color(NamedTextColor.RED));
       return true;
     }
 
     if (args.length == 0) {
       this.plugin.getDataManager().getData(player).setNickname(null);
-      sender.sendMessage(ChatColor.GREEN + "Your nickname has been reset!");
+      sender.sendMessage(
+          Component.text("Your nickname has been reset!").color(NamedTextColor.GREEN));
       return true;
     }
 
@@ -40,14 +42,15 @@ public class NickCommand implements CommandExecutor {
     }
 
     if (newNickname.length() > PlayerData.MAX_NICKNAME_LENGTH) {
-      sender.sendMessage(ChatColor.RED + "Your nickname can’t be longer than "
-          + PlayerData.MAX_NICKNAME_LENGTH + " characters");
+      sender.sendMessage(Component.text("Your nickname can’t be longer than "
+          + PlayerData.MAX_NICKNAME_LENGTH + " characters").color(NamedTextColor.RED));
       return true;
     }
 
     this.plugin.getDataManager().getData(player).setNickname(newNickname);
-    sender.sendMessage(ChatColor.GREEN + "Your nickname was set to "
-        + this.plugin.getDataManager().getData(player).getNameColor() + newNickname);
+    sender.sendMessage(Component.text("Your nickname was set to ").append(
+        Component.text(newNickname)
+            .color(this.plugin.getDataManager().getData(player).getNameColor())));
 
     return true;
   }
