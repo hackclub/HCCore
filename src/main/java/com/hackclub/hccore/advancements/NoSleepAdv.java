96fc559bb0eb4813e24b602be6e7c3623423af66
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-05-16 17:05:01 -0600

    Create NoSleepAdv.java
    no sleep
    gotten by not sleeping for a full week

---
+++ src/main/java/com/hackclub/hccore/advancements/NoSleepAdv.java
@@ -0,0 +1,32 @@
+package com.hackclub.hccore.advancements;
+
+import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
+import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplayBuilder;
+import com.fren_gor.ultimateAdvancementAPI.util.AdvancementKey;
+import com.fren_gor.ultimateAdvancementAPI.util.CoordAdapter;
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+public class SleeplessAdv extends BaseAdvancement {
+  static final AdvancementDisplayBuilder<AdvancementDisplay.Builder, AdvancementDisplay> displayBuilder =
+      new AdvancementDisplay.Builder(Material.BED, "No Sleep???")
+          .challengeFrame()
+          .announceChat()
+          .showToast()
+          .description("Stay awake for one full week");
+  static final int maxProgression = 1;
+
+  public SleeplessAdv(Advancement root, AdvancementKey key,
+      CoordAdapter adapter) {
+    super(key.getKey(), displayBuilder.coords(adapter, key).build(), root, maxProgression);
+
+    registerEvent(PlayerBedEnterEvent.class, e -> {
+      if (e.getPlayer().getStatistic(Statistic.TIME_SINCE_REST) >= 1000 * 60 * 60 * 24 * 7) {
+        grant(e.getPlayer());
+      }
+    });
+  }
+}
