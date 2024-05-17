a00dbee96d9f47704a34236537b58caf66a76fb9
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-05-16 18:02:23 -0600

    Update NoSleepAdv.java
    updated to refer to the fixes that were made by 456dev

--- src/main/java/com/hackclub/hccore/advancements/NoSleepAdv.java
+++ src/main/java/com/hackclub/hccore/advancements/NoSleepAdv.java
@@ -12,11 +12,11 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 
 public class NoSleepAdv extends BaseAdvancement {
   static final AdvancementDisplayBuilder<AdvancementDisplay.Builder, AdvancementDisplay> displayBuilder =
-      new AdvancementDisplay.Builder(Material.RED_BED, "No Sleep???")
+      new AdvancementDisplay.Builder(Material.RED_BED, "Please Sleep")
           .challengeFrame()
           .announceChat()
           .showToast()
-          .description("Stay awake for one full week");
+          .description("Go to bed after staying up for a full week");
   static final int maxProgression = 1;
 
   public NoSleepAdv(Advancement root, AdvancementKey key,
@@ -24,7 +24,7 @@ public class NoSleepAdv extends BaseAdvancement {
     super(key.getKey(), displayBuilder.coords(adapter, key).build(), root, maxProgression);
 
     registerEvent(PlayerBedEnterEvent.class, e -> {
-      if (e.getPlayer().getStatistic(Statistic.TIME_SINCE_REST) >= 1000 * 60 * 60 * 24 * 7) {
+      if (e.getPlayer().getStatistic(Statistic.TIME_SINCE_REST) >= 20 * 60 * 60 * 24 * 7) {
         grant(e.getPlayer());
       }
     });
