a4a6cb0fa03a5757aeb1573044a1ce91e6433fd9
Marios <xrteach@gmail.com>
2024-08-10 12:45:19 +0300

    feat: Update to 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "com.hackclub.hccore"
 description = "Main plugin for the Hack Club Minecraft server."
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 dependencies {
@@ -38,7 +38,7 @@ tasks {
 
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
+        options.release.set(21)
         // show deprecation warnings: paper deprecated lots of pure string functions for chat components
 //      options.isDeprecation = true
     }
@@ -59,7 +59,7 @@ tasks {
 
 
     runServer {
-        minecraftVersion("1.19.4")
+        minecraftVersion("1.21")
         systemProperty("net.kyori.adventure.text.warnWhenLegacyFormattingDetected", false)
     }
 }
