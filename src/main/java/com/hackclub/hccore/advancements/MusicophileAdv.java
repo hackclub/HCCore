da19f2f1976e201d40f61d6ac35d176fff73594c
Nicki <97249183+ProSureString@users.noreply.github.com>
2025-11-17 14:10:59 -0800

    Add 4 new music disks that are added as of MC 1.21.1

--- src/main/java/com/hackclub/hccore/advancements/MusicophileAdv.java
+++ src/main/java/com/hackclub/hccore/advancements/MusicophileAdv.java
@@ -27,13 +27,12 @@ public class MusicophileAdv extends BaseAdvancement {
           .showToast()
           .description("Collect every single music disc");
   static final Material[] musicDiscs = {Material.MUSIC_DISC_13, Material.MUSIC_DISC_CAT,
-      Material.MUSIC_DISC_BLOCKS,
-      Material.MUSIC_DISC_CHIRP, Material.MUSIC_DISC_FAR, Material.MUSIC_DISC_MALL,
-      Material.MUSIC_DISC_MELLOHI
-      , Material.MUSIC_DISC_STAL, Material.MUSIC_DISC_STRAD, Material.MUSIC_DISC_WARD,
-      Material.MUSIC_DISC_11,
+      Material.MUSIC_DISC_BLOCKS, Material.MUSIC_DISC_CHIRP, Material.MUSIC_DISC_FAR,
+      Material.MUSIC_DISC_MALL, Material.MUSIC_DISC_MELLOHI, Material.MUSIC_DISC_STAL,
+      Material.MUSIC_DISC_STRAD, Material.MUSIC_DISC_WARD, Material.MUSIC_DISC_11,
       Material.MUSIC_DISC_WAIT, Material.MUSIC_DISC_OTHERSIDE, Material.MUSIC_DISC_5,
-      Material.MUSIC_DISC_PIGSTEP};
+      Material.MUSIC_DISC_PIGSTEP, Material.MUSIC_DISC_RELIC, Material.MUSIC_DISC_CREATOR,
+      Material.MUSIC_DISC_CREATOR_MUSIC_BOX, Material.MUSIC_DISC_PRECIPICE};
 
   @SuppressWarnings("unchecked")
   public MusicophileAdv(HCCorePlugin plugin, Advancement root, AdvancementKey key,
