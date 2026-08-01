dfed449f785f639fc579ecffb6ef8cb1faff96cc
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-01 11:03:31 +1000

    Fix fall-through on invalid color in /color command and fix name reset message

--- src/main/java/com/hackclub/hccore/commands/ColorCommand.java
+++ src/main/java/com/hackclub/hccore/commands/ColorCommand.java
@@ -54,6 +54,7 @@ public class ColorCommand implements TabExecutor {
 
       if (newColor == null) {
         sender.sendMessage(InvalidColorMessage.get());
+        return true;
       }
 
 
@@ -74,7 +75,7 @@ public class ColorCommand implements TabExecutor {
       case "name" -> {
         if (args.length == 1) {
           data.setNameColor(null);
-          sender.sendMessage(ColorResetMessage.get("chat"));
+          sender.sendMessage(ColorResetMessage.get("name"));
           break;
         }
         data.setNameColor(newColor);
